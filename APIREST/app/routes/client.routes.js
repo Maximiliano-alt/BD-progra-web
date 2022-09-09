3012e2fc31f1abac857f3d743de37be86217cf73
Eliezer Zúñiga <eliezerzuniga@MacBook-Air-de-Eliezer.local>
2022-09-09 15:07:18 -0400

    backed(api-rest) added

---
+++ APIREST/app/routes/client.routes.js
@@ -0,0 +1,18 @@
+module.exports = app => {
+    const clients = require("../controllers/client.controller.js");
+    var router = require("express").Router();
+    // Crear un cliente
+    router.post("/", clients.create);
+    // buscar clientes (con condición)
+    router.get("/", clients.findAll);
+    // buscar cliente con id
+    router.get("/:id", clients.findOne);
+    // actualizar cliente
+    router.put("/:id", clients.update);
+    // eliminar cliente con id
+    router.delete("/:id", clients.delete);
+    // eliminar todos los clientes
+    router.delete("/", clients.deleteAll);
+    // agregar rutas al servidor
+    app.use('/api/clients', router);
+ };
