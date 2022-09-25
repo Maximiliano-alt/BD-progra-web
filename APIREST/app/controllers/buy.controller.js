9a3fe2bb58511a2b9eb9696520f4c34691e3dc5d
Eliezer Zúñiga <eliezerzuniga@MacBook-Air-de-Eliezer.local>
2022-09-25 19:40:54 -0300

    routes and controller buy full.

--- APIREST/app/controllers/buy.controller.js
+++ APIREST/app/controllers/buy.controller.js
@@ -32,10 +32,72 @@ exports.findAll = (req, res) =>
     const id = req.query.id_buy;
     var condition = id ? { id: { [Op.like]: `%${id}%` } } : null;
     Buy.findAll({ where: condition }) // busca las tuplas que coincida con la condición
+    
     .then(data => {
         res.send(data);
     })
     .catch(err => {
         res.status(500).send({ message: err.message || "Error en la búsqueda"});
     });
+};
+
+// Buscar una compra por su id
+exports.findOne = (req, res) => 
+{
+    const id = req.params.id_buy;
+    Buy.findByPk(id) // buscar por id
+
+    .then(data => {
+        if (data) res.send(data); // existe el dato? entrega la data
+        else      res.status(404).send({ message: `No se encontró la compra.`});
+    })
+    .catch(err => {
+        res.status(500).send({ message: "Error en la búsqueda"});
+    });
+     
+};
+
+// actualizar una compra por su id
+exports.update = (req, res) => 
+{
+    const id = req.params.id_buy;
+    Buy.update(req.body, {  where: { id_buy: id }})
+
+    .then(num => {
+        if (num == 1) res.send({ message: "Compra actualizada."});
+        else          res.send({ message: `No se pudo actualizar la compra`});
+        
+    })
+    .catch(err => {
+        res.status(500).send({ message: "Error en actualización"});
+    });
+     
+};
+
+// eliminar una compra
+exports.delete = (req, res) => 
+{
+    const id = req.params.id_buy;
+    Buy.destroy({where: { id_buy: id }})
+
+    .then(num => {
+        if (num == 1) res.send({ message: "Compra eliminada" });
+        else          res.send({ message: `Compra no encontrada`});
+    })
+    .catch(err => {
+        res.status(500).send({ message: "Error al eliminar compra"});
+    });
+};
+
+// eliminar todas las compras
+exports.deleteAll = (req, res) => 
+{
+    Buy.destroy({ where: {}, truncate: false })
+    .then(nums => {
+        res.send({ message: `${nums} Compras eliminadas!` });
+    })
+    .catch(err => {
+        res.status(500).send({ message: err.message || "Error al eliminar todas las compras." });
+    });
+    
 };
--- APIREST/app/routes/buy.routes.js
+++ APIREST/app/routes/buy.routes.js
@@ -5,6 +5,10 @@ module.exports = app =>
 
     router.post("/add", buy.create);
     router.get("/all", buy.findAll);
+    router.get("/:id_buy", buy.findOne);      // buscar buy con id
+    router.put("/:id_buy", buy.update);       // actualizar buy
+    router.delete("/:id_buy", buy.delete);    // eliminar buy con id
+    router.delete("/", buy.deleteAll);        // eliminar todas las compras
     
     app.use('/buy', router);
 }
