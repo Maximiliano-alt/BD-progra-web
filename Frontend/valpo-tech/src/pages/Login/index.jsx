ae54294fad1d1178e5dde88cd968507d5881b61f
Maximiliano-alt <74783377+Maximiliano-alt@users.noreply.github.com>
2022-10-08 09:40:48 -0300

    Basic routes added

---
+++ Frontend/valpo-tech/src/pages/Login/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import BackGround from '../../Layout/Background'
+const Login = () => {
+  return (
+    <BackGround>
+        <h1>Login</h1>
+    </BackGround>
+  )
+}
+
+export default Login
