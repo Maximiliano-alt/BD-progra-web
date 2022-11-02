5e3dfde912bcb665d83ee1439d1c5798aed30635
Eliezer Zúñiga <eliezerzuniga@MacBook-Air-de-Eliezer.local>
2022-11-02 17:48:50 -0300

    buy transaction implemented.

--- APIREST/app/models/product.model.js
+++ APIREST/app/models/product.model.js
@@ -1,13 +1,13 @@
 module.exports = (sequelize, Sequelize) =>
 {
     const Product = sequelize.define("products", {
-        id_product:     { type: Sequelize.INTEGER, allowNull: false, autoIncrement: true, primaryKey:true },
+        id_product:     { type: Sequelize.INTEGER, autoIncrement: true, allowNull: false, primaryKey: true, unique: true },
         category:       { type: Sequelize.STRING(30), allowNull: false },
         name_product:   { type: Sequelize.STRING(30), allowNull: false },
         mark:           { type: Sequelize.STRING(30), allowNull: false },
-        price:          { type: Sequelize.INTEGER, allowNull: false },
+        price:          { type: Sequelize.INTEGER.UNSIGNED, allowNull: false, validate: { min: 1 }},
         description:    { type: Sequelize.STRING(100), allowNull: true },
-        stock:          { type: Sequelize.INTEGER, allowNull: false }
+        stock:          { type: Sequelize.INTEGER.UNSIGNED, allowNull: false, validate: { min: 0 }}
     })
     return Product;
 }
--- APIREST/app/models/purchasedProduct.model.js
+++ APIREST/app/models/purchasedProduct.model.js
@@ -2,7 +2,7 @@ module.exports = (sequelize, Sequelize) =>
 {
     const PurchasedProduct = sequelize.define("purchased_products", {
         id_purchased: { type: Sequelize.INTEGER, allowNull: false, autoIncrement: true,primaryKey: true },
-        units:        { type: Sequelize.INTEGER, allowNull: false }
+        units:        { type: Sequelize.INTEGER.UNSIGNED, allowNull: false, validate: { min: 1 } }
     });
     //PurchasedProduct.removeAttribute('id'); // para eliminar el id por defecto que sequelize añade
     return PurchasedProduct;
